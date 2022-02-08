e49b5179bcb2be8717495b64369bdf6673d202cb
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-02-08 22:16:56 +0800

    ci: 🎡 use shell.js control generation husky hooks

--- shell.js
+++ shell.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 fs.exists(".husky/_", (exists) => {
   if (!exists) {
     // 添加husky hooks
-    shell.exec('pnpm husky');
-    shell.exec('npx husky add .husky/pre-commit "yarn lint-staged"');
+    shell.exec('husky install');
+    shell.exec('npx husky add .husky/pre-commit "pnpm lint-staged"');
     shell.exec('git add .husky/pre-commit');
     console.log("---------------------初始化创建`husky hooks` 成功!---------------------");
   } else {
