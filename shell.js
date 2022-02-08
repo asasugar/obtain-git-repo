0274bd5d8d87e387dcf135e663d336150d47bd0b
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-02-08 22:04:52 +0800

    fix: 🐛 fix shell.js render husky hooks

--- shell.js
+++ shell.js
@@ -1,15 +1,15 @@
 const shell = require('shelljs');
 const fs = require('fs');
 
-fs.exists(".husky", (exists) => {
+fs.exists(".husky/_", (exists) => {
   if (!exists) {
     // 添加husky hooks
     shell.exec('pnpm husky');
     shell.exec('npx husky add .husky/pre-commit "yarn lint-staged"');
     shell.exec('git add .husky/pre-commit');
-    console.log("初始化创建创建`.husky`文件夹成功!");
+    console.log("---------------------初始化创建`husky hooks` 成功!---------------------");
   } else {
-    console.log(".husky已存在");
+    console.log("---------------------husky hooks已存在,不重复生成-------------------");
   }
 });
 
